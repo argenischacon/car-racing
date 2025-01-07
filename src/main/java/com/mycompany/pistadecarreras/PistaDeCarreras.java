e83f3665e19a7f1476d932c5232a13363abb737b
Argenis Chacon B. <argenischaconb@gmail.com>
2025-01-07 22:52:51 +0000

    fist commit

---
+++ src/main/java/com/mycompany/pistadecarreras/PistaDeCarreras.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.pistadecarreras;
+
+
+public class PistaDeCarreras {
+
+    public static void main(String[] args) {
+       
+        
+        
+    }
+}
---
+++ src/main/java/gui/Race.java
@@ -0,0 +1,20 @@
+
+package gui;
+
+import javax.swing.JFrame;
+
+
+public class Race extends JFrame{
+    
+    public Race(){
+        
+        
+        
+        setSize(600, 900);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+    
+    
+}
