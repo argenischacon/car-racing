8b14a3701053587e91179fd8496dc3f5bf2e79df
Argenis Chacon B. <argenischaconb@gmail.com>
2025-01-07 22:58:34 +0000

    Create Race instance in main method

--- src/main/java/com/mycompany/pistadecarreras/PistaDeCarreras.java
+++ src/main/java/com/mycompany/pistadecarreras/PistaDeCarreras.java
@@ -1,12 +1,14 @@
 
 package com.mycompany.pistadecarreras;
 
+import gui.Race;
+
 
 public class PistaDeCarreras {
 
     public static void main(String[] args) {
        
-        
+       Race race = new Race();
         
     }
 }
--- src/main/java/gui/Race.java
+++ src/main/java/gui/Race.java
@@ -1,15 +1,20 @@
 
 package gui;
 
+import java.awt.GridBagLayout;
 import javax.swing.JFrame;
 
 
 public class Race extends JFrame{
     
     public Race(){
+        //Establecemos el layout
+        setLayout(new GridBagLayout());
         
         
         
+        //establecemos la configuracion de la ventana
+        pack();
         setSize(600, 900);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
