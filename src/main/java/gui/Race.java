8dc3012cb682037d3c61a71862cbf92be28a2bb0
Argenis Chacon B. <argenischaconb@gmail.com>
2025-01-08 21:45:02 +0000

    feat: initComponents method

--- src/main/java/gui/Race.java
+++ src/main/java/gui/Race.java
@@ -1,20 +1,24 @@
 
 package gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
 
 public class Race extends JFrame{
     
     public Race(){
-        //Establecemos el layout
-        GridBagLayout gridbag = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-        setLayout(gridbag);
-        
-        
+        initComponents();
         
         //establecemos la configuracion de la ventana
         pack();
@@ -23,6 +27,118 @@ public class Race extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         
+    }
+
+    private void initComponents() {
+        JPanel panelPrincipal = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        
+        //Boton de start (JButton)
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0;// No ocupa espacio adicional en X
+        gbc.weighty = 0;// No ocupa espacio adicional en Y
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        JButton btnStart = new JButton("Start");
+        btnStart.setPreferredSize(new Dimension(150, 150));
+        panelPrincipal.add(btnStart, gbc);
+        
+        //Etiqueta para indicar seleccion de auto (JLabel)
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0, 10, 0, 10);
+        JLabel lblIndicarSeleccion = new JLabel("Seleccione su auto: ");
+        lblIndicarSeleccion.setFont(new Font("Product Sans", Font.BOLD, 15));
+        lblIndicarSeleccion.setPreferredSize(new Dimension(150, 30));
+        lblIndicarSeleccion.setHorizontalAlignment(JLabel.CENTER);
+        panelPrincipal.add(lblIndicarSeleccion, gbc);
+        
+        //Botones para seleccionar auto (JToggleButton)
+        
+        //Boton del auto rojo
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        JToggleButton btnRedCar = new JToggleButton();
+        btnRedCar.setPreferredSize(new Dimension(150, 150));
+        panelPrincipal.add(btnRedCar, gbc);
+        
+        //Boton del auto verde
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        JToggleButton btnGreenCar = new JToggleButton();
+        btnGreenCar.setPreferredSize(new Dimension(150, 150));
+        panelPrincipal.add(btnGreenCar, gbc);
+        
+        //Boton del auto azul
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        JToggleButton btnBlueCar = new JToggleButton();
+        btnBlueCar.setPreferredSize(new Dimension(150, 150));
+        panelPrincipal.add(btnBlueCar, gbc);
+        
+        //Grupo de botones (ButtonGroup)
+        ButtonGroup group = new ButtonGroup();
+        group.add(btnRedCar);
+        group.add(btnGreenCar);
+        group.add(btnBlueCar);
+        
+        //Label para la pista
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridheight = 5;
+        gbc.weightx = 1;//Ocupa espacio adicional en x
+        gbc.weighty = 1;//Ocupa espacio adicional en y
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets(0, 0, 0, 0);
+        JLabel lblTrack = new JLabel();
+        lblTrack.setLayout(null);
+        lblTrack.setOpaque(true);
+        lblTrack.setBackground(Color.BLACK);
+        
+        //Labels para los autos (JLabel)
+        JLabel lblRedCar = new JLabel();
+        lblRedCar.setBounds(50, 650, 50, 100);
+        lblRedCar.setOpaque(true);
+        lblRedCar.setBackground(Color.red);
+        lblTrack.add(lblRedCar);
+        
+        JLabel lblGreenCar = new JLabel();
+        lblGreenCar.setBounds(190, 650, 50, 100);
+        lblGreenCar.setOpaque(true);
+        lblGreenCar.setBackground(Color.GREEN);
+        lblTrack.add(lblGreenCar);
+        
+        JLabel lblBlueCar = new JLabel();
+        lblBlueCar.setBounds(330, 650, 50, 100);
+        lblBlueCar.setOpaque(true);
+        lblBlueCar.setBackground(Color.BLUE);
+        lblTrack.add(lblBlueCar);
+        
+        panelPrincipal.add(lblTrack, gbc);
+        
+        //Añadimos el panel principal a la ventana
+        this.add(panelPrincipal);
     }
     
     
